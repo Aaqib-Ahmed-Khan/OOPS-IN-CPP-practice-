e325090a85624fdd571788fa7b71d8acd23c5a17
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-03-02 19:46:12 +0500

    user defined data type

---
+++ UserDefinedDataType.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class student{
+public:
+    string name;
+    int rno;
+    float gpa;
+}
+int main (){
+    Student s;
+    s.name="aaqib"
+    s.rno=62;
+    s.gpa=4.0;
+
+    Student s1;
+    s.name="ahmed"
+    s.rno=62;
+    s.gpa=4.0;
+}
