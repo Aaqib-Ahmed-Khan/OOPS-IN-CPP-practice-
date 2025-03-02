03423c30f72476d7e88bf8bb4085ee13d33cca37
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-03-02 19:52:09 +0500

    small corrections

--- UserDefinedDataType.cpp
+++ UserDefinedDataType.cpp
@@ -5,15 +5,17 @@ public:
     string name;
     int rno;
     float gpa;
-}
+};
 int main (){
-    Student s;
-    s.name="aaqib"
+    student s;
+    s.name="aaqib";
     s.rno=62;
     s.gpa=4.0;
 
-    Student s1;
-    s.name="ahmed"
-    s.rno=62;
-    s.gpa=4.0;
+    student s1;
+    s1.name="ahmed";
+    s1.rno=62;
+    s1.gpa=4.0;
+cout<<s.name<<endl;
+return 0;
 }
